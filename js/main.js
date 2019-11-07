82dea0e6414faad6bc26f1457e4c0e980cbb8006
Ruhi-1 <56201885+Ruhi-1@users.noreply.github.com>
2019-11-07 13:33:47 -0600

    updated play button and made dot moving inside table. now i will work on setting score and timer

--- js/main.js
+++ js/main.js
@@ -1,72 +1,48 @@
 /*----- constants -----*/
-gameLength = 20;
+const gameLength = 20;
 timerID = null
-var playing = false;
-var currentpos = -1;
-var numholes = 6 * 10;
+let playing = false;
+let currentpos = -1;
+let numholes = 6 * 10;
 function clear() {
-    for(var i=0; i<document.rtp.elements.length; i++)
-    document.rtp.elements[i].checked=false;
+    for(let i=0; i<document.rtp.elements.length; i++)
+    document.rtp.elements[i].checked=true;
 }
 
-
-
-
-
-
-
-/*----- app's state (variables) -----*/
-
-
-
 /*----- cached element references -----*/
-var total = document.getElementById("circle");
-// var dot = document.getElementByClassName("dots");
-var playBtnClick = document.getElementById('reset');
-// console.log(playBtnClick);
-
-
-
-
-
-
-
-
+const total = document.getElementById("circle");
+const playBtnClick = document.getElementById('reset');
 
 /*----- event listeners -----*/
-document.getElementById('reset')
+playBtnClick
     .addEventListener("click", makeId);
-// document.getElementById('circle')
-//     .addEventListener("click", movesMade);
-
-
 
 /*----- functions -----*/
 
-function makeId()
+function checkButton()
     {
-    var randomNumber=Math.floor(Math.random() * 60);
-    
-    var dot = document.getElementById(randomNumber);
-    
-    dot.checked = true;
-    console.log(dot);
-}
-// function movesMade() {
-//     var randomNumber=Math.floor(Math.random() * dot)
-//     console.log(randomNumber);
-// }
+      playBtnClick.disabled = true;
+      return true;
+    }
 
-function hithead(evt) {
-    var dot=Math.floor(Math.random() * 60);
+function makeId() {
+    var randomNumber=Math.floor(Math.random() * numholes);
+    var dot = document.getElementById(randomNumber);
+    console.log('makeId', dot)
+    console.log('makeId', randomNumber)
+    dot.checked = true;
+    // console.log(dot);
+    checkButton();
+};
+
+function hithead(id) {
+    const oldDot = document.getElementById(id)
+    oldDot.checked = false
+    var dot=Math.floor(Math.random() * numholes);
     var dot = document.getElementById(dot)
     dot.checked = true;
-    console.log(dot)
+    // console.log(dot)
 }
-
-
-
-
 
 
 

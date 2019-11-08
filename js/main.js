59c094b068f9bb70decb35135fa0de90830c70b7
Ruhi-1 <56201885+Ruhi-1@users.noreply.github.com>
2019-11-07 22:37:03 -0600

    finished project

--- js/main.js
+++ js/main.js
@@ -7,10 +7,6 @@ let oldDot = '';
 let playing = false;
 let currentpos = -1;
 let numholes = 6 * 10;
-// function clear(){
-//     for(let i=0; i<document.box.total; i++)
-//     document.box[i].checked=true;
-// }
 
 /*----- cached element references -----*/
 const total = document.getElementById("circle");
@@ -18,21 +14,17 @@ const playBtnClick = document.getElementById('reset');
 const table = document.getElementById('grab');
 /*----- event listeners -----*/
 playBtnClick.addEventListener("click", makeId, startPlay);
-table.addEventListener('click', scoreBtn);
 
 /*----- functions -----*/
 
-
-function checkButton()
-    {
-      playBtnClick.disabled = true;
-      return true;
-    }
+function checkButton() {
+    playBtnClick.disabled = true;
+    return true;
+}
 
 function makeId() {
     var randomNumber=Math.floor(Math.random() * numholes);
     dot = document.getElementById(randomNumber);
-    // console.log(dot);
     dot.checked = true;
     checkButton();
     startPlay();
@@ -42,34 +34,38 @@ function hithead(id) {
     dot.checked = false;
     oldDot = document.getElementById(id)
     oldDot.checked = false
+    scoreBtn(dot.id == oldDot.id);
     dot=Math.floor(Math.random() * numholes);
     dot = document.getElementById(dot)
     dot.checked = true;
-    // scoreBtn();
 }
 
-function startPlay(){
-var timeleft = 0;
-var downloadTimer = setInterval(function(){
-    timeleft += 1;
-     document.getElementById("record").value = timeleft;
-    if(timeleft == 20){
-        clearInterval(downloadTimer);
-        stopgame();
-    }
+function startPlay() {
+    var timeleft = 0;
+    var downloadTimer = setInterval(function(){
+        timeleft += 1;
+        document.getElementById("record").value = timeleft;
+        if(timeleft == 20){
+            clearInterval(downloadTimer);
+            stopgame();
+        }
 }, 1000)};
 
-function scoreBtn(evt) {
-    if (evt.target.id === oldDot.id) {
+function scoreBtn(userHit) {
+    if (userHit) {
         score++;
     } else {
-        score--;
+        score == 0 ? 0 : score--;
     }
+    document.getElementsByClassName('scorecard')[0].value = score;
 };
+
 function stopgame() {
     playing=false;
     document.game.timeleft=0;
-    alert('Game Over.');
+    alert('Game Over. Your Score: ' + score);
+    playBtnClick.disabled = false;
+      return false;
 }
 
 
